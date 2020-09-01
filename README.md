3b97d802d3f699684b9c1138a800e54521b1ff04
adityarege22 <70090303+adityarege22@users.noreply.github.com>
2020-09-01 13:58:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Collision-of-Algorithm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(200, 200, 50, 80);
+fixedRect.shapeColor = "brown";
+
+  movingRect = createSprite(400, 200, 80, 30);
+movingRect.shapeColor = "orange";
+
+}
+
+function draw() {
+  background(255,255,255);
+  
+if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
+  && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+  && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+  && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+  fixedRect.shapeColor = "red";
+  movingRect.shapeColor = "red";
+}
+else
+{
+  fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+} 
+
+
+
+
+  movingRect.y = World.mouseY;
+  movingRect.x = World.mouseX;
+ 
+
+  drawSprites();
+}
